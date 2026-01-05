3728bba836202fb18fee97ecb9c144a21049767c
robin <89062704+Robib611126@users.noreply.github.com>
2026-01-05 12:52:29 +0530

    Switch to BrowserRouter to remove hashes from URLs

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 
 const rootElement = document.getElementById('root');
@@ -14,9 +14,9 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-      <HashRouter>
+      <BrowserRouter>
         <App />
-      </HashRouter>
+      </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
 );
