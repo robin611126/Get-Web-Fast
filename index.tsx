fee8c70fcf31d438a5ea0bdd125a47991a5467e0
robin <89062704+Robib611126@users.noreply.github.com>
2026-02-20 23:05:46 +0530

    feat: Integrate Lenis for smooth scrolling by adding the library, configuring it with custom RAF, and updating CSS.

--- index.tsx
+++ index.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { ReactLenis } from 'lenis/react';
+import { useAnimationFrame } from 'framer-motion';
+
+function SmoothScrolling({ children }: { children: React.ReactNode }) {
+  const lenisRef = useRef<any>(null);
+
+  useAnimationFrame((time) => {
+    if (lenisRef.current?.lenis) {
+      lenisRef.current.lenis.raf(time);
+    }
+  });
+
+  return (
+    <ReactLenis
+      root
+      ref={lenisRef}
+      autoRaf={false}
+      options={{ lerp: 0.05, wheelMultiplier: 1.2, smoothWheel: true, syncTouch: true, touchMultiplier: 2 }}
+    >
+      {children}
+    </ReactLenis>
+  );
+}
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
@@ -15,7 +38,9 @@ root.render(
   <React.StrictMode>
     <HelmetProvider>
       <BrowserRouter>
-        <App />
+        <SmoothScrolling>
+          <App />
+        </SmoothScrolling>
       </BrowserRouter>
     </HelmetProvider>
   </React.StrictMode>
